67f9ab401d91b5d6df3e4ffef896c0cdb355b5de
Bernd P <bernd.preissmann@gmx.de>
2012-02-19 02:18:23 +0100

    erste erstellung

---
+++ linksammlung.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Linksammlung
+Plugin URI: http://debaernd.de
+Description: Automatisches Bloggen von Linksammlungen
+Version: 0.0
+Author: deBaernd
+Author URI: http://deBaernd
+License: GPL
+Stable Tag: 0.0
+*/
+?>
